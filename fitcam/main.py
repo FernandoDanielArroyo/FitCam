64bcef3d56b1df63b4c90e9fb78bc9bfbaf55268
raphael_campus <raphael.bouvet@le-campus-numerique.fr>
2023-03-28 11:55:59 +0200

    add argparse test to main script :)

--- fitcam/main.py
+++ fitcam/main.py
@@ -1,3 +1,16 @@
+import argparse
+
+def parse():
+    parser = argparse.ArgumentParser(
+        prog='Yoga FitCam'
+    )
+    parser.add_argument('-i', '--input_file', default='webcam')
+    parser.add_argument('-o', '--output_file', default='No output')
+    parser.add_argument('-d', '--display', action='store_true')
+    args = parser.parse_args()
+    return args
 
 def main():
-    print('test poetry script')
+    args = parse()
+    print(f'test poetry script')
+    print(args)
