ec1f21dee8e6dc85a948d762d5b88df83646de5d
raphael_campus <raphael.bouvet@le-campus-numerique.fr>
2023-03-28 12:16:18 +0200

    add more in main script
    shows cam or video depending on args

--- fitcam/main.py
+++ fitcam/main.py
@@ -1,4 +1,6 @@
 import argparse
+import cv2
+import numpy as np
 
 def parse():
     parser = argparse.ArgumentParser(
@@ -14,3 +16,44 @@ def main():
     args = parse()
     print(f'test poetry script')
     print(args)
+
+
+    if args.input_file == 'webcam':
+        print('Loading WebCam')
+        cap = cv2.VideoCapture(0)
+        try:
+            while cap.isOpened():
+                success, input_frame = cap.read()
+                output_frame = input_frame.copy()
+                cv2.imshow('MediaPipe', np.array(output_frame)[:,:,::-1])
+
+                
+                if cv2.waitKey(5) & 0xFF == ord('q'):
+                    break
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()   
+    else:
+        print('Loading Video')
+        try:
+            cap = cv2.VideoCapture(args.input_file)
+            frame_id = 0
+            video_n_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            video_fps = cap.get(cv2.CAP_PROP_FPS)
+            video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            for i in range(int(video_n_frames)):
+                success, input_frame = cap.read()
+                output_frame = input_frame.copy()
+                cv2.imshow('MediaPipe', np.array(output_frame)[:,:,::-1])
+                if cv2.waitKey(5) & 0xFF == ord('q'):
+                        break
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()   
+        
+
+
+
+
+
